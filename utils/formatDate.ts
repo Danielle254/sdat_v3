3e9bc812f520f483e7ecd6e128b8ff006acab5cf
Danielle Lindblom <danielle.lindblom@gmail.com>
2025-03-10 09:23:16 -0600

    refactor: prettier

--- utils/formatDate.ts
+++ utils/formatDate.ts
@@ -1,12 +1,14 @@
 export default function formatDate(input: string): string {
-    
-    const visitYear = input.slice(0, 4);
-    let visitMonth = input.slice(5, 7);
-    if(visitMonth[0] === '0') {visitMonth = visitMonth[1]}  
-    let visitDay = input.slice(8);
+  const visitYear = input.slice(0, 4);
+  let visitMonth = input.slice(5, 7);
+  if (visitMonth[0] === "0") {
+    visitMonth = visitMonth[1];
+  }
+  let visitDay = input.slice(8);
 
-    if(visitDay[0] === '0') {visitDay = visitDay[1]};
+  if (visitDay[0] === "0") {
+    visitDay = visitDay[1];
+  }
 
-    return `${visitMonth}/${visitDay}/${visitYear}`;
+  return `${visitMonth}/${visitDay}/${visitYear}`;
 }
-
