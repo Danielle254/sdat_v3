dc9f4d07d8beb70e43519e53a69ff229171d6ba2
Danielle Lindblom <danielle.lindblom@gmail.com>
2025-03-10 12:22:46 -0600

    feat: create material UI navbar

---
+++ components/NavBar.tsx
@@ -0,0 +1,33 @@
+import * as React from "react";
+import AppBar from "@mui/material/AppBar";
+import Box from "@mui/material/Box";
+import Toolbar from "@mui/material/Toolbar";
+import Typography from "@mui/material/Typography";
+import IconButton from "@mui/material/IconButton";
+import MenuIcon from "@mui/icons-material/Menu";
+import Image from "next/image";
+import logo from "../public/logo.png";
+
+export default function ButtonAppBar() {
+  return (
+    <Box sx={{ flexGrow: 1 }}>
+      <AppBar position="static">
+        <Toolbar>
+          <Image src={logo} height={40} alt="" width={40} />
+          <Typography variant="h6" component="div" sx={{ flexGrow: 1 }}>
+            Service Dogs Around Town
+          </Typography>
+          <IconButton
+            size="large"
+            edge="start"
+            color="inherit"
+            aria-label="menu"
+            sx={{ mr: 2 }}
+          >
+            <MenuIcon />
+          </IconButton>
+        </Toolbar>
+      </AppBar>
+    </Box>
+  );
+}
