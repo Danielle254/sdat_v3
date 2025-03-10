176a2ce03e47ec5273dd9cfc062bd946e76f88af
Danielle Lindblom <danielle.lindblom@gmail.com>
2025-03-10 12:23:14 -0600

    refactor: prettier

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -1,7 +1,6 @@
 import type { Metadata } from "next";
 import "./globals.css";
 
-
 export const metadata: Metadata = {
   title: "Service Dogs Around Town",
   description: "project by Danielle Lindblom",
@@ -14,9 +13,7 @@ export default function RootLayout({
 }>) {
   return (
     <html lang="en">
-      <body>
-        {children}
-      </body>
+      <body>{children}</body>
     </html>
   );
 }
