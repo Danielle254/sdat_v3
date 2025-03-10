d917d8205f86334fa5831d7361ec994793c2eb7d
Danielle Lindblom <danielle.lindblom@gmail.com>
2025-03-10 11:44:53 -0600

    feat: add map to page (google maps api)

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,7 +1,32 @@
-import styles from "./page.module.css";
+"use client";
+
+import { useState } from "react";
+import { APIProvider, Map } from "@vis.gl/react-google-maps";
 
 export default function Home() {
+  const [zoom, setZoom] = useState(4);
+  const [position, setPosition] = useState({ lat: 40, lng: -97 });
+
   return (
-    <></>
+    <APIProvider apiKey={process.env.NEXT_PUBLIC_MAPS_API_KEY as string}>
+      <div className="map-container">
+        <Map
+          zoom={zoom}
+          center={position}
+          mapId={process.env.NEXT_PUBLIC_MAPS_ID as string}
+          reuseMaps={true}
+          onCenterChanged={(map) => setPosition(map.detail.center)}
+          onZoomChanged={(map) => setZoom(map.detail.zoom)}
+          gestureHandling={"greedy"}
+          disableDefaultUI={true}
+          zoomControl={true}
+          fullscreenControl={true}
+          clickableIcons={false}
+          zoomControlOptions={{
+            position: 6,
+          }}
+        ></Map>
+      </div>
+    </APIProvider>
   );
 }
