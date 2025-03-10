02eb3ff6c5885c6a8c19e1ef14eab09de31a4ecd
Danielle Lindblom <danielle.lindblom@gmail.com>
2025-03-10 13:00:23 -0600

    refactor: move map to separate component for better code readability

---
+++ components/DisplayMap.tsx
@@ -0,0 +1,34 @@
+"use client";
+
+import { useState } from "react";
+import { APIProvider, Map } from "@vis.gl/react-google-maps";
+
+import React from "react";
+
+export default function DisplayMap() {
+  const [zoom, setZoom] = useState(4);
+  const [position, setPosition] = useState({ lat: 40, lng: -97 });
+  return (
+    <APIProvider apiKey={process.env.NEXT_PUBLIC_MAPS_API_KEY as string}>
+      <div className="map-container">
+        <Map
+          zoom={zoom}
+          center={position}
+          mapId={process.env.NEXT_PUBLIC_MAPS_ID as string}
+          reuseMaps={true}
+          onCenterChanged={(map) => setPosition(map.detail.center)}
+          onZoomChanged={(map) => setZoom(map.detail.zoom)}
+          gestureHandling={"greedy"}
+          disableDefaultUI={true}
+          zoomControl={true}
+          fullscreenControl={true}
+          clickableIcons={false}
+          zoomControlOptions={{
+            position: 6,
+          }}
+          fullscreenControlOptions={{ position: 6 }}
+        ></Map>
+      </div>
+    </APIProvider>
+  );
+}
