c93599e6879a22debf87d2e84d70583b9eca2543
Danielle Lindblom <danielle.lindblom@gmail.com>
2025-03-11 08:26:45 -0600

    style: create custom material ui theme

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -1,4 +1,5 @@
 import type { Metadata } from "next";
+import ThemeRegistry from "../../theme/ThemeRegistry";
 import "./globals.css";
 
 export const metadata: Metadata = {
@@ -13,7 +14,9 @@ export default function RootLayout({
 }>) {
   return (
     <html lang="en">
-      <body>{children}</body>
+      <ThemeRegistry>
+        <body>{children}</body>
+      </ThemeRegistry>
     </html>
   );
 }
---
+++ theme/ThemeRegistry.tsx
@@ -0,0 +1,31 @@
+"use client";
+
+import * as React from "react";
+import CssBaseline from "@mui/material/CssBaseline";
+import { createTheme, ThemeOptions, ThemeProvider } from "@mui/material/styles";
+import { NextAppDirEmotionCacheProvider } from "./EmotionCache";
+
+const themeOptions: ThemeOptions = {
+  palette: {
+    primary: {
+      main: "#0E1B41",
+    },
+  },
+};
+
+const theme = createTheme(themeOptions);
+
+export default function ThemeRegistry({
+  children,
+}: {
+  children: React.ReactNode;
+}) {
+  return (
+    <NextAppDirEmotionCacheProvider options={{ key: "mui" }}>
+      <ThemeProvider theme={theme}>
+        <CssBaseline />
+        {children}
+      </ThemeProvider>
+    </NextAppDirEmotionCacheProvider>
+  );
+}
