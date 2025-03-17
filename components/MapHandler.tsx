69039994256c1f50bec9117a0a7eebd341f2ee9e
Danielle Lindblom <danielle.lindblom@gmail.com>
2025-03-17 10:55:51 -0600

    feat: map handler set up with TS

---
+++ components/MapHandler.tsx
@@ -0,0 +1,20 @@
+import { useEffect } from "react";
+import { useMap } from "@vis.gl/react-google-maps";
+
+type MapHandlerProps = {
+  place: google.maps.places.PlaceResult | null;
+  marker: google.maps.marker.AdvancedMarkerElement | null;
+};
+
+export default function MapHandler({ place, marker }: MapHandlerProps) {
+  const map = useMap();
+
+  useEffect(() => {
+    if (!map || !place || !marker) return;
+    if (place.geometry?.viewport) {
+      map.fitBounds(place.geometry?.viewport);
+    }
+    marker.position = place.geometry?.location;
+  }, [map, place, marker]);
+  return null;
+}
