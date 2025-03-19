ddb46e58ebfb5c78084e877eb964207b7a8f4427
Danielle Lindblom <danielle.lindblom@gmail.com>
2025-03-18 18:43:17 -0600

    feat: add a drawer component for the navigation menu

--- components/NavBar.tsx
+++ components/NavBar.tsx
@@ -1,14 +1,25 @@
-import * as React from "react";
+"use client";
+
+import { useState } from "react";
 import AppBar from "@mui/material/AppBar";
 import Box from "@mui/material/Box";
 import Toolbar from "@mui/material/Toolbar";
 import Typography from "@mui/material/Typography";
 import IconButton from "@mui/material/IconButton";
+import Drawer from "@mui/material/Drawer";
 import MenuIcon from "@mui/icons-material/Menu";
 import Image from "next/image";
 import logo from "../public/logo.png";
+import ChevronRightIcon from "@mui/icons-material/ChevronRight";
+import NavLinksList from "./NavLinksList";
+import Divider from "@mui/material/Divider";
 
 export default function NavBar() {
+  const [open, setOpen] = useState(false);
+
+  const toggleDrawer = (newOpen: boolean) => () => {
+    setOpen(newOpen);
+  };
   return (
     <Box sx={{ flexGrow: 1 }}>
       <AppBar position="static">
@@ -34,9 +45,29 @@ export default function NavBar() {
             color="inherit"
             aria-label="menu"
             sx={{ mr: 2 }}
+            onClick={toggleDrawer(true)}
           >
             <MenuIcon />
           </IconButton>
+          <Drawer open={open} anchor="right" onClose={toggleDrawer(false)}>
+            <Box sx={{ width: 250 }} role="presentation">
+              <IconButton
+                size="large"
+                color="inherit"
+                aria-label="menu"
+                onClick={toggleDrawer(false)}
+                sx={{
+                  display: "flex",
+                  flexDirection: "row",
+                  justifyContent: "flex-end",
+                }}
+              >
+                <ChevronRightIcon />
+              </IconButton>
+              <Divider />
+              <NavLinksList />
+            </Box>
+          </Drawer>
         </Toolbar>
       </AppBar>
     </Box>
