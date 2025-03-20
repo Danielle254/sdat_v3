c5235fd35d1769674d9f449af9de2b61800dcb2c
Danielle Lindblom <danielle.lindblom@gmail.com>
2025-03-20 12:02:52 -0600

    feat: create reuseable modal component

--- components/ModalBox.tsx
+++ components/ModalBox.tsx
@@ -1,5 +1,41 @@
 import React from "react";
+import Modal from "@mui/material/Modal";
+import NewPlaceForm from "./NewPlaceForm";
+import Box from "@mui/material/Box";
 
-export default function ModalBox() {
-  return <div></div>;
+type ModalBoxProps = {
+  modalOpen: boolean;
+  handleCloseModal: () => void;
+};
+
+export default function ModalBox({
+  modalOpen,
+  handleCloseModal,
+}: ModalBoxProps) {
+  return (
+    <Modal
+      open={modalOpen}
+      onClose={handleCloseModal}
+      aria-labelledby="modal-modal-title"
+      aria-describedby="modal-modal-description"
+    >
+      <Box
+        sx={{
+          position: "absolute",
+          top: "50%",
+          left: "50%",
+          transform: "translate(-50%, -50%)",
+          width: "50%",
+          bgcolor: "background.paper",
+          border: "2px solid #000",
+          boxShadow: 24,
+          p: 4,
+          maxHeight: "80%",
+          overflowY: "scroll",
+        }}
+      >
+        <NewPlaceForm />
+      </Box>
+    </Modal>
+  );
 }
