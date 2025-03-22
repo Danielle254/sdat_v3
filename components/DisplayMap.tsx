b9e15c00132d545e6c4c02ce40cf19731e9b9d65
Danielle Lindblom <danielle.lindblom@gmail.com>
2025-03-21 19:34:21 -0600

    feat: add uuid package to assign id at new place creation

--- components/DisplayMap.tsx
+++ components/DisplayMap.tsx
@@ -1,6 +1,6 @@
 "use client";
 
-import { useState, useCallback } from "react";
+import { useState, useCallback, useEffect } from "react";
 import {
   AdvancedMarker,
   APIProvider,
@@ -14,17 +14,41 @@ import MapHandler from "./MapHandler";
 import React from "react";
 import { Box, Button } from "@mui/material";
 import Typography from "@mui/material/Typography";
-import ModalBox from "./ModalBox";
+import NewPlace from "./NewPlace";
+import { entriesCollection } from "../api/firebase";
+import { collection, doc, getDocs } from "firebase/firestore";
+import type { NewPlaceType, ExistingPlaceType } from "../types/place";
 
 export default function DisplayMap() {
-  const [zoom, setZoom] = useState(4);
+  const [zoom, setZoom] = useState(5);
   const [position, setPosition] = useState({ lat: 40, lng: -97 });
   const [markerRef, marker] = useAdvancedMarkerRef();
   const [selectedPlace, setSelectedPlace] =
-    useState<google.maps.places.PlaceResult | null>(null);
+    useState<google.maps.places.PlaceResult>();
+  const [activeMarker, setActiveMarker] = useState<ExistingPlaceType | null>(
+    null
+  );
   const [infoWindowShown, setInfoWindowShown] = useState(true);
   const handleClose = useCallback(() => setInfoWindowShown(false), []);
   const [modalOpen, setModalOpen] = useState(false);
+  const [places, setPlaces] = useState<ExistingPlaceType[]>([]);
+
+  async function getData() {
+    const querySnapshot = await getDocs(entriesCollection);
+    const entriesArr: any = [];
+    querySnapshot.forEach((doc) => {
+      entriesArr.push({ ...doc.data(), id: doc.id });
+    });
+    setPlaces(entriesArr);
+  }
+
+  useEffect(() => {
+    getData();
+  }, []);
+
+  function handleActiveMarker(id: string) {
+    setActiveMarker(places[places.findIndex((each) => each.id === id)]);
+  }
 
   return (
     <APIProvider apiKey={process.env.NEXT_PUBLIC_MAPS_API_KEY as string}>
@@ -89,27 +113,46 @@ export default function DisplayMap() {
               </Box>
             </InfoWindow>
           )}
-          <AdvancedMarker
-            position={{
-              lat: 46.8360851,
-              lng: -114.0154287,
-            }}
-          >
-            <Pin
-              background={"#0E1B41"}
-              borderColor={"#0E1B41"}
-              glyphColor={"#53cbe2"}
-              scale={1.2}
-            />
-          </AdvancedMarker>
+          {places.map((place) => (
+            <AdvancedMarker
+              position={place.coords}
+              key={place.id}
+              clickable={true}
+              onClick={() => handleActiveMarker(place.id)}
+            >
+              <Pin
+                background={"#0E1B41"}
+                borderColor={"#0E1B41"}
+                glyphColor={"#53cbe2"}
+                scale={1.2}
+              />
+            </AdvancedMarker>
+          ))}
+          {activeMarker && (
+            <InfoWindow
+              position={activeMarker.coords}
+              onCloseClick={() => setActiveMarker(null)}
+            >
+              <div>
+                <p>{activeMarker.name}</p>
+                <p>{/* {cityState(activeMarker.address)} */}</p>
+                <button
+                /* onClick={() => openModal(activeMarker.id)} */
+                >
+                  View Details
+                </button>
+              </div>
+            </InfoWindow>
+          )}
         </Map>
         <MapHandler place={selectedPlace} marker={marker} />
       </div>
-
-      <ModalBox
+      <NewPlace
         modalOpen={modalOpen}
         handleCloseModal={() => setModalOpen(false)}
-        selectedPlace={selectedPlace}
+        name={selectedPlace?.name}
+        address={selectedPlace?.formatted_address}
+        coords={selectedPlace?.geometry?.location}
       />
     </APIProvider>
   );
