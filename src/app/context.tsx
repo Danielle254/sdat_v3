0851bcbda9e1e11a633370a89322233c0f45a76c
Danielle Lindblom <danielle.lindblom@gmail.com>
2025-03-24 14:01:52 -0600

    refactor: bring user and data management functions to top level of app

--- src/app/context.tsx
+++ src/app/context.tsx
@@ -3,13 +3,21 @@
 import { createContext } from "react";
 import { useState, useEffect } from "react";
 import { GoogleAuthProvider, signInWithPopup } from "firebase/auth";
-import { auth } from "../../api/firebase";
+import { doc, setDoc, getDocs, deleteDoc } from "firebase/firestore";
+import { auth, entriesCollection } from "../../api/firebase";
+import type { PlaceType } from "../../types/place";
 
 type MapContextType = {
   isLoggedIn: boolean;
   userId: string;
   googleLogin: () => void;
   handleLogout: () => void;
+  places: PlaceType[];
+  addPlace: (
+    e: React.FormEvent<HTMLFormElement>,
+    place: PlaceType
+  ) => Promise<void>;
+  deletePlace: (id: string) => Promise<void>;
 };
 
 export const MapContext = createContext<MapContextType>({
@@ -17,6 +25,9 @@ export const MapContext = createContext<MapContextType>({
   userId: "",
   googleLogin: () => {},
   handleLogout: () => {},
+  places: [],
+  addPlace: () => new Promise(() => {}),
+  deletePlace: () => new Promise(() => {}),
 });
 
 export function MapContextProvider({
@@ -26,6 +37,7 @@ export function MapContextProvider({
 }) {
   const [isLoggedIn, setIsLoggedIn] = useState(false);
   const [userId, setUserId] = useState(null);
+  const [places, setPlaces] = useState<PlaceType[]>([]);
 
   function googleLogin() {
     const provider = new GoogleAuthProvider();
@@ -51,13 +63,48 @@ export function MapContextProvider({
     setIsLoggedIn(false);
   }
 
+  async function getData() {
+    const querySnapshot = await getDocs(entriesCollection);
+    const entriesArr: any = [];
+    querySnapshot.forEach((doc) => {
+      entriesArr.push(doc.data());
+    });
+    const convertedData: PlaceType[] = entriesArr;
+    setPlaces(convertedData);
+  }
+
+  async function addPlace(
+    e: React.FormEvent<HTMLFormElement>,
+    place: PlaceType
+  ) {
+    e.preventDefault();
+    const docRef = await setDoc(doc(entriesCollection, place.id), place);
+    setPlaces([...places, place]);
+  }
+
+  async function deletePlace(id: string) {
+    const docRef = doc(entriesCollection, id);
+    await deleteDoc(docRef);
+    const newPlaces = places.filter((place) => place.id !== id);
+    setPlaces(newPlaces);
+  }
+
   useEffect(() => {
     checkIsLoggedIn();
+    getData();
   }, []);
 
   return (
     <MapContext.Provider
-      value={{ isLoggedIn, userId, googleLogin, handleLogout }}
+      value={{
+        isLoggedIn,
+        userId,
+        googleLogin,
+        handleLogout,
+        places,
+        addPlace,
+        deletePlace,
+      }}
     >
       {children}
     </MapContext.Provider>
