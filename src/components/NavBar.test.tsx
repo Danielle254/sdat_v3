17a6bc417194df4eec231c4a55a467ca1b3443b9
Danielle Lindblom <danielle.lindblom@gmail.com>
2025-06-04 13:00:41 -0600

    feat: create placeholder files for nav elements tests

---
+++ src/components/NavBar.test.tsx
@@ -0,0 +1,5 @@
+// test drawer opens/closes
+// keyboard navigable
+// confirm h1
+// header title has correct href
+// logo has empty alt text
---
+++ src/components/NavLinksList.test.tsx
@@ -0,0 +1,6 @@
+// test that the they are links
+// test that they have the right hrefs
+// confirm login/out is a button
+// service dog laws should open in a new window
+// confirm that icons are aria-hidden
+// list is keyboard navigable
